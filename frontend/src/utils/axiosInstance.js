7ef3a2bf43a339a798af393c8c88942cf20789b3
riteshpaarihar <ritesh.rs199@gmail.com>
2025-05-23 10:51:10 +0530

    feat(admin-dashboard): complete analytics dashboard with visual insights and data integration

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API_BASE_URL =
-    import.meta.env.VITE_API_URL || "https://vloging-production.up.railway.app/api";
+    // import.meta.env.VITE_API_URL || "https://vloging-production.up.railway.app/api";
+    import.meta.env.VITE_API_URL || "https://vloging.onrender.com/api";
 //import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 // ✅ GOOD PART
 const axiosInstance = axios.create({
